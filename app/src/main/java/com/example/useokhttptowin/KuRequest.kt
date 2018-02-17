5afa99a33030124d3ecc8826f957e8951c579a3b
guzongjia <kabuto412rock@gmail.com>
2018-02-17 15:26:19 +0800

    這是第二版的use OkHttp to Win， 介面xml的textView修改成有scroll bar可以捲動。
    並且上方有兩個按鈕，
    
    左邊按鈕可以發送request，將內容利用handleJsonStr函數更新我們的
    textView內容(有將我們的json字串處理成物件，只印出我們想看的屬性)。
    
    右邊按鈕可以清空textView內容

--- app/src/main/java/com/example/useokhttptowin/KuRequest.kt
+++ app/src/main/java/com/example/useokhttptowin/KuRequest.kt
@@ -1,5 +1,6 @@
 package com.example.useokhttptowin
 
+import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
@@ -11,9 +12,9 @@ class KuRequest {
         val request = Request.Builder()
                 .url(url)
                 .build()
-
         val response = client.newCall(request).execute()
         return response.body()?.string()
     }
+
 
 }
