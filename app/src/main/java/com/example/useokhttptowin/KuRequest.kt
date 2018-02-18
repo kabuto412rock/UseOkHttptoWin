295decdec597e0ca2baf0cafe8c5c88eb2f793bb
guzongjia <kabuto412rock@gmail.com>
2018-02-18 09:54:57 +0800

    嘗試gitignore一些檔案

--- app/src/main/java/com/example/useokhttptowin/KuRequest.kt
+++ app/src/main/java/com/example/useokhttptowin/KuRequest.kt
@@ -1,11 +1,14 @@
 package com.example.useokhttptowin
 
+import android.os.Build
 import com.google.gson.Gson
+import com.google.gson.JsonObject
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
 class KuRequest {
     private val client = OkHttpClient()
+    private val gson = Gson()
 
     // 發送一個get請求
     fun get(url: String): String? {
@@ -16,5 +19,9 @@ class KuRequest {
         return response.body()?.string()
     }
 
+    fun <T>getJson(url: String, classOfT: Class<T>): T {
+        val jsonStr = get(url)
+        return gson.fromJson(jsonStr, classOfT)
 
+    }
 }
--- app/src/main/java/com/example/useokhttptowin/MainActivity.kt
+++ app/src/main/java/com/example/useokhttptowin/MainActivity.kt
@@ -1,76 +1,19 @@
 package com.example.useokhttptowin
 
-import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.text.method.ScrollingMovementMethod
-import android.util.Log
-import android.widget.TextView
-import com.example.useokhttptowin.R.id.textview_show_json
-import com.google.gson.Gson
-import com.google.gson.JsonArray
-import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.ctx
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.runOnUiThread
-import org.jetbrains.anko.sdk25.coroutines.onClick
-import org.jetbrains.anko.uiThread
+import okhttp3.OkHttpClient
 
 
 class MainActivity : AppCompatActivity() {
-
+    val KuRequest = KuRequest()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 讓textview能有scroll bar能往下滑
-        textview_show_json.movementMethod = ScrollingMovementMethod()
+        val jsonApiUrl = ""
+        KuRequest.getJson(jsonApiUrl, )
 
-        btn_gson_test.onClick {
-            val testJsonUrl = "http://jsonplaceholder.typicode.com/Users"
-            val kuRequest = KuRequest()
-            doAsync {
-                val jsonStr = kuRequest.get(testJsonUrl)
-
-                jsonStr?.let {
-                    handleJsonstr(it, textview_show_json)
-                }
-            }
-        }
-
-        btn_clear_textview.onClick {
-            textview_show_json.text = ""
-        }
-    }
-
-
-
-}
-
-class User(val name: String,
-           val email: String,
-           val address: UserAddress,
-           val company: UserComany)
-class UserAddress(val city: String)
-class UserComany(val name: String)
-
-fun Context.handleJsonstr(jsonStr: String, textView: TextView) {
-    val gson = Gson()
-    val userArray = gson.fromJson(jsonStr, JsonArray::class.java)
-    val stringBuilder = StringBuilder()
-    for (user in userArray) {
-        val userObj = user.asJsonObject
-
-        val userId = userObj.get("id")
-        val userCityAddress = userObj.getAsJsonObject("address").get("city")
-        val userCompanyName = userObj.getAsJsonObject("company").get("name")
-        val userName = userObj["name"]
-
-        stringBuilder.append("$userId. 名稱:$userName, 城市:$userCityAddress, 公司:$userCompanyName\n\n")
-    }
-
-    runOnUiThread {
-        textView.text = stringBuilder.toString()
     }
 
 }
